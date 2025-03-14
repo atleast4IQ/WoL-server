1dda18da42950ab1f61142c28e076220282d33e2
atleast4IQ <131665427+atleast4IQ@users.noreply.github.com>
2025-03-14 11:40:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 def wake_on_lan(mac_address):
     try:
         send_magic_packet(mac_address)
-        return f'WOL an {mac_address} sent! <script>window.close()</script>'
+        return f'WOL to {mac_address} sent! <script>window.close()</script>'
     except Exception as e:
-        return f'Fehler: {str(e)}', 400
+        return f'Error: {str(e)}', 400
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
